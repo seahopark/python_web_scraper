b0efecc25298b76f9bd946c1707a4b1ea0d47a03
Antony Park | 박세호 <afloat@outlook.com>
2017-10-09 02:45:44 +0900

    init
    init

--- scraping.py
+++ scraping.py
@@ -1,1 +1,5 @@
-import requests
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+html = urlopen("https://www.ddengle.com")
+bsObj = BeautifulSoup(html.read(),"html.parser")
+print(bsObj.h1)
